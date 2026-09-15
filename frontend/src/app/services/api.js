529f1121bf3ae33f6f6fc05c6975e1dd8cd34a3f
shaurya pandey <shauryapandey336@gmail.com>
2026-09-15 12:36:58 +0530

    live

--- frontend/src/app/services/api.js
+++ frontend/src/app/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: "http://127.0.0.1:5000/api",
+     "https://resume-analyzer-by1r.onrender.com"
     headers: {
         "Content-Type": "application/json",
     },
